980593bd898915c52a382a0aad34919590df64c8
Brian Emilius <brian@brianemilius.com>
2018-12-01 21:32:30 +0100

    add mysql library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.0] - 2018-12-01
+### Added
+* Bibliotek til at køre forskellige myswl-operationer.
+
+### Changed
+* MySQL opsætningen har fået et fallback så den ikke længere udelukkende er afhængig af `.env`-variablerne.
+
 ## [0.2.1] - 2018-11-21
 ### Added
 * Denne changelog
--- config/mysql.js
+++ config/mysql.js
@@ -2,9 +2,9 @@ module.exports = (function () {
     const mysql = require('mysql');
     return () => mysql.createPool({
         'connectionLimit': 10,
-        'host': process.env.DB_HOST,
-        'user': process.env.DB_USER,
-        'password': process.env.DB_PSWD,
-        'database': process.env.DB_DTBS
+        'host': process.env.DB_HOST || 'localhost',
+        'user': process.env.DB_USER || 'root',
+        'password': process.env.DB_PSWD || 'root',
+        'database': process.env.DB_DTBS || 'test'
     });
 }());
