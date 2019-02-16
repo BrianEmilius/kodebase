b369db0037114e0eecdc4855221c39e9fdbbddc4
Brian Emilius <brian@brianemilius.com>
2019-02-16 21:08:16 +0100

    add routes for signup/in process

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,19 @@
 const Query = require('../lib/queries');
 const users = new Query('users');
+const bcrypt = require('bcrypt');
 
 module.exports = function (app) {
 	app.post('/auth/signin', async (req, res, next) => {
 		try {
-			const result = await users.findOne({ email: req.fields.username, secret: req.fields.password });
-			if (result.length === 1) {
+			const result = await users.findOne({ email: req.fields.email });
+			if (result.length !== 1) return res.redirect('/signin');
+
+			if (bcrypt.compareSync(req.fields.password, result[0].secret)) {
+				if (!result[0].active) return res.redirect(`/verify/${result[0].id}`);
+
 				req.session.user = result[0].id;
 				res.redirect('/');
-			} else {
-				res.redirect('/signin');
-				return;
-			}
+			} else return res.redirect('/signin');
 		} catch(err) {
 			next(err);
 		}
