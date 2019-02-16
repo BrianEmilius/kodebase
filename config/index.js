c23313f010fc2f8eb05cbe9112e0b1ea65b9e5dd
Brian Emilius <brian@brianemilius.com>
2019-02-16 21:06:08 +0100

    add error handling to auto-loaders

--- config/index.js
+++ config/index.js
@@ -4,7 +4,11 @@ const path = require('path');
 module.exports = function (app) {
     fs.readdirSync(__dirname, { withFileTypes: true }).forEach(file => {
         if (file.name !== path.basename(__filename)) {
-            require(path.join(__dirname, file.name))(app);
+            try {
+                require(path.join(__dirname, file.name))(app);
+            } catch(error) {
+                console.error(error);
+            }
         }
     });
 };
