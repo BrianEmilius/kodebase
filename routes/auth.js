4a4f43915d2a91df8769320a043bc7b8129310c8
Brian Emilius <brian@brianemilius.com>
2019-02-18 19:14:13 +0100

    add flash message to singin

--- routes/auth.js
+++ routes/auth.js
@@ -6,14 +6,20 @@ module.exports = function (app) {
 	app.post('/auth/signin', async (req, res, next) => {
 		try {
 			const result = await users.findOne({ email: req.fields.email });
-			if (result.length !== 1) return res.redirect('/signin');
+			if (result.length !== 1) {
+				req.flash('error', 'Invalid username or password');
+				return res.redirect('/signin');
+			}
 
 			if (bcrypt.compareSync(req.fields.password, result[0].secret)) {
 				if (!result[0].active) return res.redirect(`/verify/${result[0].id}`);
 
 				req.session.user = result[0].id;
 				res.redirect('/');
-			} else return res.redirect('/signin');
+			} else  {
+				req.flash('error', 'Invalid username or password');
+				return res.redirect('/signin');
+			}
 		} catch(err) {
 			next(err);
 		}
