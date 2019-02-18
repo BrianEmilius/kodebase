348e1e050fdf379f068da95e23d18618e4fe4db7
Brian Emilius <brian@brianemilius.com>
2019-02-18 19:13:48 +0100

    add support for flash messages

--- config/session.js
+++ config/session.js
@@ -1,9 +1,18 @@
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
 
 module.exports = function (app) {
+    app.use(cookieParser(process.env.SESSION_SECRET));
+
     app.use(session({
-        'resave': false,
-        'saveUninitialized': true,
-        'secret': process.env.SESSION_SECRET || 'blowfishsecret'
+        resave: false,
+        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET || 'blowfishsecret',
+        cookie: {
+            maxAge: 60000
+        }
     }));
+
+    app.use(flash());
 };
