8b516dc8aeafc68a57e57f4ec45e536b76023150
Brian Emilius <brian@brianemilius.com>
2019-03-15 10:18:41 +0100

    add i18n middleware and configuration

--- config/session.js
+++ config/session.js
@@ -1,15 +1,17 @@
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('express-flash');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("express-flash");
 
-module.exports = function (app) {
-    app.use(cookieParser(process.env.SESSION_SECRET));
+module.exports = function(app) {
+  app.use(cookieParser(process.env.SESSION_SECRET));
 
-    app.use(session({
-        resave: false,
-        saveUninitialized: true,
-        secret: process.env.SESSION_SECRET || 'blowfishsecret'
-    }));
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: true,
+      secret: process.env.SESSION_SECRET
+    })
+  );
 
-    app.use(flash());
+  app.use(flash());
 };
--- error-handling/routing.js
+++ error-handling/routing.js
@@ -2,17 +2,20 @@ module.exports = function(app) {
   app.use((req, res) => {
     res.status(404);
     res.render("errorpage", {
-      title: "404: Not Found",
-      content:
+      title: res.__("404: Not Found"),
+      content: res.__(
         "The page you are looking for does not exist. This service is either temporarily unable to process your request or the link you clicked is broken. Please check back shortly."
+      )
     });
   });
 
   app.use((error, req, res, next) => {
     res.status(500);
     res.render("errorpage", {
-      title: "500: Internal Server Error",
-      content: "Something went wrong on your server. Check your logs."
+      title: res.__("500: Internal Server Error"),
+      content: res.__(
+        "Something went wrong on the server. The error has been logged."
+      )
     });
     next(error);
   });
